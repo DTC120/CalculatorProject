60b7081ed41ad8cb0a5599d814a55926a946a31f
diegotrejo <dietrej12@gmail.com>
2025-05-31 12:34:36 -0600

    Initial Commit

---
+++ CalculatorProject/CalculatorProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorProjectApp.swift
+//  CalculatorProject
+//
+//  Created by Diego Trejo on 31/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
